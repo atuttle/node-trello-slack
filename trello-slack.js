90ed7ff42266dc7044cad68a03e7570d34d61b63
Adam Tuttle <adam@fusiongrokker.com>
2014-03-04 13:34:28 -0500

    fix redis connection syntax

--- trello-slack.js
+++ trello-slack.js
@@ -25,7 +25,7 @@ function bootstrap(callback){
 		if (process.env.REDISTOGO_URL) {
 			var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 			redis = require("redis").createClient(rtg.port, rtg.hostname);
-			redis.client.auth(rtg.auth.split(":")[1]);
+			redis.auth(rtg.auth.split(":")[1]);
 		} else {
 			redis = require("redis").createClient();
 		}

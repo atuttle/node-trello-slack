8fac0bb5027a05ceaed54c927fcb0b9ff416fa7c
Adam Tuttle <adam@fusiongrokker.com>
2014-03-11 19:12:30 -0400

    refactored to fix some race conditions because async!

--- trello-slack.js
+++ trello-slack.js
@@ -7,7 +7,7 @@ var fs = require('fs')
 var cfg = require('./config.json');
 var trello = new Trello(cfg.trello.auth.key, cfg.trello.auth.token);
 var slack = new Slack(cfg.slack.domain, cfg.slack.token);
-var redis, prevId, board, actions, msg, cachedCardLists = {};
+var redis, prevId, handlers, cachedCardLists = {};
 var mechanism = 'file';
 
 bootstrap(function(prev){
@@ -41,113 +41,124 @@ function bootstrap(callback){
 }
 
 function watch(){
+	var board;
 	for (var ix in cfg.trello.boards){
 		board = cfg.trello.boards[ix];
-
-		trello.get('/1/boards/' + board.id + '/actions', function(err, resp){
-			if (err) throw err;
-			// console.log(resp[0]);
-			var actions = resp.reverse();
-
-			for (var aix in actions){
-				//only concern ourselves with new entries
-				var A = actions[aix];
-				var actionId = parseInt(A.id, 16);
-				if (actionId <= prevId){
-					continue;
-				}
-
-				if (A.type === "commentCard"){
-					// console.log(A.data.text);
-					// process.exit(1);
-				   card_in_lists(A.data.card.id, board.lists, A, function(doNotify, B){
-				   	if (doNotify){
-							var card_id_short = B.data.card.idShort
-						      ,card_id = B.data.card.id
-						      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-						      ,card_name = B.data.card.name
-							   ,author = B.memberCreator.fullName
-							   ,msg = ':speech_balloon: ' + author + ' commented on card <' + card_url + '|'
-							    + card_name + '>: ' + trunc(B.data.text);
-							notify(board.slack_channel || cfg.slack.default_channel, msg);
-				   	}
-				   });
-				}else if (A.type === 'addAttachmentToCard'){
-					card_in_lists(A.data.card.id, board.lists, A, function(doNotify, B){
-						if (doNotify){
-							var card_id_short = B.data.card.idShort
-						      ,card_id = B.data.card.id
-						      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-						      ,card_name = B.data.card.name
-							   ,author = B.memberCreator.fullName
-							   ,aurl = B.data.attachment.url;
-							var m = ':paperclip: ' + author + ' added an attachment to card <'
-							      + card_url + '|' + card_name + '>: '
-							      + '<' + aurl + '|' + B.data.attachment.name + '>';
-							notify(board.slack_channel || cfg.slack.default_channel, m);
-							if (aurl){
-								var aurllower = aurl.toLowerCase();
-								if (aurllower.match(/.+(\.png|\.gif|\.jpg|\.jpeg)$/)){
-									notify(board.slack_channel || cfg.slack.default_channel, aurl);
-								}
-							}
-						}
-					});
-				}else if (A.type === 'updateCard'){
-					if (A.data.old.hasOwnProperty('idList') && A.data.card.hasOwnProperty('idList')){
-						//moving between lists
-						encapsulate(A, function(B){
-							var oldId = B.data.old.idList;
-							var newId = B.data.card.idList;
-							var nameO,nameN;
-							var card_id_short = B.data.card.idShort
-						      ,card_id = B.data.card.id
-						      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-						      ,card_name = B.data.card.name
-							   ,author = B.memberCreator.fullName
-							   ;
-							trello.get('/1/list/' + oldId, function(err, resp){
-								if (err) throw err;
-								nameO = resp.name;
-								trello.get('/1/list/' + newId, function(err, resp){
-									if (err) throw err;
-									nameN = resp.name;
-									if (board.lists.indexOf(nameO) > -1 || board.lists.indexOf(nameN) > -1){
-										var msg = ':arrow_heading_up:' + author + ' moved card <'
-										        + card_url + '|' + card_name + '> from list '
-										        + nameO + ' to list ' + nameN;
-										notify(board.slack_channel || cfg.slack.default_channel, msg);
-									}
-								});
-							});
-						});
-					}
-				}else if (A.type === 'updateCheckItemStateOnCard'){
-					card_in_lists(A.data.card.id, board.lists, A, function(doNotify, B){
-						if (doNotify){
-							var card_id_short = B.data.card.idShort
-						      ,card_id = B.data.card.id
-						      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-						      ,card_name = B.data.card.name
-							   ,author = B.memberCreator.fullName
-							   ;
-							if (B.data.checkItem.state === 'complete'){
-								var msg = ':ballot_box_with_check: ' + author + ' completed "'
-								+ B.data.checkItem.name + '" in card <' + card_url + '|' + card_name + '>.';
-								notify(board.slack_channel || cfg.slack.default_channel, msg);
-							}
-						}
-					});
-				}
-
-				prevId = Math.max(actionId, prevId);
-				writePrevId(prevId);
-			}
-		});
+		getBoardActivity(board);
 	}
 
 	setTimeout(watch, 1000*60*3);
 }
+
+function getBoardActivity(board){
+	console.log('getting board activity', board);
+	trello.get('/1/boards/' + board.id + '/actions', function(err, resp){
+		if (err) throw err;
+		// console.log(resp[0]);
+		var actions = resp.reverse();
+
+		for (var aix in actions){
+			//only concern ourselves with new entries
+			var A = actions[aix];
+			var actionId = parseInt(A.id, 16);
+			if (actionId <= prevId){
+				continue;
+			}
+
+			if (handlers.hasOwnProperty(A.type)){
+				var hndl = handlers[A.type];
+				hndl(A, board);
+			}
+
+			prevId = Math.max(actionId, prevId);
+			writePrevId(prevId);
+		}
+	});
+}
+
+handlers = {
+	commentCard: function(A, board){
+	   card_in_lists(A.data.card.id, board.lists, function(doNotify){
+	   	if (doNotify){
+				var card_id_short = A.data.card.idShort
+			      ,card_id = A.data.card.id
+			      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
+			      ,card_name = A.data.card.name
+				   ,author = A.memberCreator.fullName
+				   ,msg = ':speech_balloon: ' + author + ' commented on card <' + card_url + '|'
+				    + card_name + '>: ' + trunc(A.data.text);
+				notify(board.slack_channel || cfg.slack.default_channel, msg);
+	   	}
+	   });
+	}
+	,addAttachmentToCard: function(A, board){
+		card_in_lists(A.data.card.id, board.lists, function(doNotify){
+			if (doNotify){
+				var card_id_short = A.data.card.idShort
+			      ,card_id = A.data.card.id
+			      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
+			      ,card_name = A.data.card.name
+				   ,author = A.memberCreator.fullName
+				   ,aurl = A.data.attachment.url;
+				var m = ':paperclip: ' + author + ' added an attachment to card <'
+				      + card_url + '|' + card_name + '>: '
+				      + '<' + aurl + '|' + A.data.attachment.name + '>';
+				notify(board.slack_channel || cfg.slack.default_channel, m);
+				if (aurl){
+					var aurllower = aurl.toLowerCase();
+					if (aurllower.match(/.+(\.png|\.gif|\.jpg|\.jpeg)$/)){
+						notify(board.slack_channel || cfg.slack.default_channel, aurl);
+					}
+				}
+			}
+		});
+	}
+	,updateCard: function(A, board){
+		if (A.data.old.hasOwnProperty('idList') && A.data.card.hasOwnProperty('idList')){
+			//moving between lists
+			var oldId = A.data.old.idList;
+			var newId = A.data.card.idList;
+			var nameO,nameN;
+			var card_id_short = A.data.card.idShort
+		      ,card_id = A.data.card.id
+		      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
+		      ,card_name = A.data.card.name
+			   ,author = A.memberCreator.fullName
+			   ;
+			trello.get('/1/list/' + oldId, function(err, resp){
+				if (err) throw err;
+				nameO = resp.name;
+				trello.get('/1/list/' + newId, function(err, resp){
+					if (err) throw err;
+					nameN = resp.name;
+					if (board.lists.indexOf(nameO) > -1 || board.lists.indexOf(nameN) > -1){
+						var msg = ':arrow_heading_up:' + author + ' moved card <'
+						        + card_url + '|' + card_name + '> from list '
+						        + nameO + ' to list ' + nameN;
+						notify(board.slack_channel || cfg.slack.default_channel, msg);
+					}
+				});
+			});
+		}
+	}
+	,updateCheckItemStateOnCard: function(A, board){
+		card_in_lists(A.data.card.id, board.lists, function(doNotify){
+			if (doNotify){
+				var card_id_short = A.data.card.idShort
+			      ,card_id = A.data.card.id
+			      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
+			      ,card_name = A.data.card.name
+				   ,author = A.memberCreator.fullName
+				   ;
+				if (A.data.checkItem.state === 'complete'){
+					var msg = ':ballot_box_with_check: ' + author + ' completed "'
+					+ A.data.checkItem.name + '" in card <' + card_url + '|' + card_name + '>.';
+					notify(board.slack_channel || cfg.slack.default_channel, msg);
+				}
+			}
+		});
+	}
+};
 
 function notify(room, msg, sender){
 	sender = sender || 'Trello';
@@ -162,10 +173,10 @@ function notify(room, msg, sender){
 		}
 	});
 }
-function card_in_lists(card_id, lists, A, callback){
+function card_in_lists(card_id, lists, callback){
 	//use cache if it exists
 	if (cachedCardLists.hasOwnProperty(card_id)){
-		return callback(cachedCardLists[card_id], A);
+		return callback(cachedCardLists[card_id]);
 	}
 	//otherwise get the list name
 	trello.get('/1/cards/' + card_id + '/list', function(err, resp){
@@ -174,7 +185,7 @@ function card_in_lists(card_id, lists, A, callback){
 
 		var found = (lists.indexOf('*') > -1 || lists.indexOf(list_name) > -1);
 		cachedCardLists[card_id] = found;
-		callback(found, A);
+		callback(found);
 	});
 }
 function encapsulate(A,cb){

cc4698769a775d9e4ec5c1044f30807e12484694
Adam Tuttle <adam@fusiongrokker.com>
2014-03-22 11:14:27 -0400

    rewritten to use trello-events module and be a useable module itself

--- ReadMe.md
+++ ReadMe.md
@@ -1,30 +1,57 @@
 # node-trello-slack
 
-The built-in integration for Trello provided by Slack/SlackHQ stinks. It's limited to one board, which of course can only post activity updates to 1 channel.
+The built-in integration for Trello provided by Slack/SlackHQ isn't enough. It's limited to one board!
 
-This tool will check the trello api once a minute for updates and push them into your desired channels. You can configure any number of boards that you want, and each board's activity can be posted into any channel of your choosing.
+This tool will check the trello api once a minute for updates and push them into your desired channels. You can configure any number of boards.
 
-## Getting access to Trello and Slack
+### Install
 
-You'll need a Trello key and token. [Get your key here](https://trello.com/1/appKey/generate): it's the one in the box near the top labeled "key." Once you have that key, substitute it into the following url for <KEY-HERE> and open it up in a browser tab:
+	npm install --save node-trello-slack
 
-    https://trello.com/1/connect?name=MyApp&response_type=token&expiration=never&key=<KEY-HERE>
+### Usage
 
-Fill the key and token into config.json.
+Create an executable script with the following code:
 
-You'll also need your webhook token and domain name for Slack. The domain name is just the part of the url before ".slack.com." To get your token, go to the following url (substituting your domain for <YOUR-DOMAIN>) and add the webhook integration. The token will be listed in the left sidebar. Fill that into config.json as well.
+```js
+#!/usr/bin/env node
+
+var Bot = require('./trello-slack')
+	,bot = new Bot({
+		pollFrequency: 1000*60*3 //every 3 minutes
+		,trello: {
+			boards: ['Nz5nyqZg','...']
+			,key: 'trello-key-here'
+			,token: 'trello-token-here'
+		}
+		,slack: {
+			domain: 'slack-domain-here'
+			,token: 'slack-webhook-token-here'
+			,channel: '#general'
+		}
+	});
+```
+
+## Getting your Trello and Slack credentials
+
+You'll need a **Trello key and token.** [Get your key here](https://trello.com/1/appKey/generate): it's the one in the box near the top labeled "key." Once you have that key, substitute it into the following url for <KEY-HERE> and open it up in a browser tab:
+
+    https://trello.com/1/connect?name=node-trello-slack&response_type=token&expiration=never&key=<KEY-HERE>
+
+You'll also need your webhook **token and domain** for Slack. The domain is just the part of the url before `.slack.com`. To get your token, go to the following url (substituting your domain for <YOUR-DOMAIN>) and add the webhook integration (if it's not already enabled). The token will be listed in the left sidebar.
+
+Fill all four of these values into your bot config, and tweak the other options
 
 ## Running...
 
-### ...locally (or where the file system is writeable)
-
 Once you've configured access to your Trello and Slack accounts, the last thing to know is how this tool knows what events it's already seen. There are two options: File system, and Redis.
 
-The simplest is using the file system. Just create a file named `last.id` in the root folder of the project and put the number `0` into it.
+### ...locally (or where the file system is writeable)
+
+The simplest is using the file system. Just create a file named `last.id` in the root folder of the project and put the number `0` into it. As long as the filesystem is writeable the file will be updated when new board actions have been seen.
 
 ### ...on Heroku (or where the file system is not writeable)
 
-I run this tool on Heroku (a single free dyno works great!) but it doesn't allow you to write to the file system. Instead, I use Redis. Add the free **Redis To Go** addon and everything should just work out of the box.
+I run this tool on Heroku (a single free dyno works great!) but they dont allow you to write to the filesystem. Instead, I use Redis. Add the free **Redis To Go** addon and everything should just work out of the box.
 
     heroku create
     heroku addons:add redistogo
@@ -35,6 +62,24 @@ This will push whatever you've got committed in your local git repo up to a new 
 
 Enjoy!
 
-# License
+# The MIT License (MIT)
 
-This code is released under the MIT license.
+> Copyright (c) 2014 Adam Tuttle
+
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+> The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- trello-slack.js
+++ trello-slack.js
@@ -1,20 +1,35 @@
-#!/usr/bin/env node
-
 var fs = require('fs')
-   ,Trello = require('node-trello')
+   ,Trello = require('trello-events')
    ,Slack = require('node-slack');
 
-var cfg = require('./config.json');
-var trello = new Trello(cfg.trello.auth.key, cfg.trello.auth.token);
-var slack = new Slack(cfg.slack.domain, cfg.slack.token);
-var redis, prevId, handlers, cachedCardLists = {};
+var cfg, trello, slack, redis, prevId, handlers;
 var mechanism = 'file';
 
-bootstrap(function(prev){
-	prevId = prev;
-	watch();
-});
+module.exports = function(config){
+	cfg = config;
 
+	bootstrap(function(prev){
+		cfg.minId = prev;
+		slack = new Slack(cfg.slack.domain, cfg.slack.token);
+		trello = new Trello(cfg);
+
+		trello
+			.on('maxId', writePrevId)
+			.on('trelloError', function(err){
+				console.error(err);
+				process.exit(1);
+			})
+			.on('commentCard', handlers.commentCard)
+			.on('addAttachmentToCard', handlers.addAttachmentToCard)
+			.on('updateCard', handlers.updateCard)
+			.on('updateCheckItemStateOnCard', handlers.updateCheckItemStateOnCard)
+	});
+}
+
+
+/*
+	handles the choice between redis and local files
+*/
 function bootstrap(callback){
 	//if we can find a file named "last.id" then use that to store the activity timeline bookmark
 	if (fs.existsSync('./last.id')){
@@ -40,117 +55,65 @@ function bootstrap(callback){
 	}
 }
 
-function watch(){
-	var board;
-	for (var ix in cfg.trello.boards){
-		board = cfg.trello.boards[ix];
-		getBoardActivity(board);
-	}
-
-	setTimeout(watch, 1000*60*3);
-}
-
-function getBoardActivity(board){
-	console.log('getting board activity', board);
-	trello.get('/1/boards/' + board.id + '/actions', function(err, resp){
-		if (err) throw err;
-		// console.log(resp[0]);
-		var actions = resp.reverse();
-
-		for (var aix in actions){
-			//only concern ourselves with new entries
-			var A = actions[aix];
-			var actionId = parseInt(A.id, 16);
-			if (actionId <= prevId){
-				continue;
-			}
-
-			if (handlers.hasOwnProperty(A.type)){
-				var hndl = handlers[A.type];
-				hndl(A, board);
-			}
-
-			prevId = Math.max(actionId, prevId);
-			writePrevId(prevId);
-		}
-	});
-}
-
 handlers = {
-	commentCard: function(A, board){
-	   card_in_lists(A.data.card.id, board.lists, function(doNotify){
-	   	if (doNotify){
-				var card_id_short = A.data.card.idShort
-			      ,card_id = A.data.card.id
-			      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-			      ,card_name = A.data.card.name
-				   ,author = A.memberCreator.fullName
-				   ,msg = ':speech_balloon: ' + author + ' commented on card <' + card_url + '|'
-				    + sanitize(card_name) + '>: ' + trunc(A.data.text);
-				notify(board.slack_channel || cfg.slack.default_channel, msg);
-	   	}
-	   });
+	commentCard: function(event, boardId){
+		var card_id_short = event.data.card.idShort
+			,card_id = event.data.card.id
+			,card_url = 'https://trello.com/card/' + card_id + '/' + boardId + '/' + card_id_short
+			,card_name = event.data.card.name
+			,author = event.memberCreator.fullName
+			,msg = ':speech_balloon: ' + author + ' commented on card <' + card_url + '|'
+				  + sanitize(card_name) + '>: ' + trunc(event.data.text);
+		notify(cfg.slack.channel, msg);
 	}
-	,addAttachmentToCard: function(A, board){
-		card_in_lists(A.data.card.id, board.lists, function(doNotify){
-			if (doNotify){
-				var card_id_short = A.data.card.idShort
-			      ,card_id = A.data.card.id
-			      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-			      ,card_name = A.data.card.name
-				   ,author = A.memberCreator.fullName
-				   ,aurl = A.data.attachment.url;
-				var m = ':paperclip: ' + author + ' added an attachment to card <'
-				      + card_url + '|' + sanitize(card_name) + '>: '
-				      + '<' + aurl + '|' + sanitize(A.data.attachment.name) + '>';
-				notify(board.slack_channel || cfg.slack.default_channel, m);
-			}
-		});
+	,addAttachmentToCard: function(event, boardId){
+		var card_id_short = event.data.card.idShort
+			,card_id = event.data.card.id
+			,card_url = 'https://trello.com/card/' + card_id + '/' + boardId + '/' + card_id_short
+			,card_name = event.data.card.name
+			,author = event.memberCreator.fullName
+			,aurl = event.data.attachment.url;
+		var msg = ':paperclip: ' + author + ' added an attachment to card <'
+			   + card_url + '|' + sanitize(card_name) + '>: '
+			   + '<' + aurl + '|' + sanitize(event.data.attachment.name) + '>';
+		notify(cfg.slack.channel, msg);
 	}
-	,updateCard: function(A, board){
-		if (A.data.old.hasOwnProperty('idList') && A.data.card.hasOwnProperty('idList')){
+	,updateCard: function(event, boardId){
+		if (event.data.old.hasOwnProperty('idList') && event.data.card.hasOwnProperty('idList')){
 			//moving between lists
-			var oldId = A.data.old.idList;
-			var newId = A.data.card.idList;
-			var nameO,nameN;
-			var card_id_short = A.data.card.idShort
-		      ,card_id = A.data.card.id
-		      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-		      ,card_name = A.data.card.name
-			   ,author = A.memberCreator.fullName
-			   ;
-			trello.get('/1/list/' + oldId, function(err, resp){
+			var oldId = event.data.old.idList
+				,newId = event.data.card.idList
+				,nameO,nameN
+				,card_id_short = event.data.card.idShort
+		      ,card_id = event.data.card.id
+		      ,card_url = 'https://trello.com/card/' + card_id + '/' + boardId + '/' + card_id_short
+		      ,card_name = event.data.card.name
+			   ,author = event.memberCreator.fullName;
+			trello.api.get('/1/list/' + oldId, function(err, resp){
 				if (err) throw err;
 				nameO = resp.name;
-				trello.get('/1/list/' + newId, function(err, resp){
+				trello.api.get('/1/list/' + newId, function(err, resp){
 					if (err) throw err;
 					nameN = resp.name;
-					if (board.lists.indexOf(nameO) > -1 || board.lists.indexOf(nameN) > -1){
-						var msg = ':arrow_heading_up:' + author + ' moved card <'
-						        + card_url + '|' + sanitize(card_name) + '> from list '
-						        + nameO + ' to list ' + nameN;
-						notify(board.slack_channel || cfg.slack.default_channel, msg);
-					}
+					var msg = ':arrow_heading_up:' + author + ' moved card <'
+					        + card_url + '|' + sanitize(card_name) + '> from list '
+					        + nameO + ' to list ' + nameN;
+					notify(cfg.slack.channel, msg);
 				});
 			});
 		}
 	}
-	,updateCheckItemStateOnCard: function(A, board){
-		card_in_lists(A.data.card.id, board.lists, function(doNotify){
-			if (doNotify){
-				var card_id_short = A.data.card.idShort
-			      ,card_id = A.data.card.id
-			      ,card_url = 'https://trello.com/card/' + card_id + '/' + board.id + '/' + card_id_short
-			      ,card_name = A.data.card.name
-				   ,author = A.memberCreator.fullName
-				   ;
-				if (A.data.checkItem.state === 'complete'){
-					var msg = ':ballot_box_with_check: ' + author + ' completed "'
-					+ A.data.checkItem.name + '" in card <' + card_url + '|' + sanitize(card_name) + '>.';
-					notify(board.slack_channel || cfg.slack.default_channel, msg);
-				}
-			}
-		});
+	,updateCheckItemStateOnCard: function(event, boardId){
+		var card_id_short = event.data.card.idShort
+			,card_id = event.data.card.id
+			,card_url = 'https://trello.com/card/' + card_id + '/' + boardId + '/' + card_id_short
+			,card_name = event.data.card.name
+			,author = event.memberCreator.fullName;
+		if (event.data.checkItem.state === 'complete'){
+			var msg = ':ballot_box_with_check: ' + author + ' completed "'
+					  + event.data.checkItem.name + '" in card <' + card_url + '|' + sanitize(card_name) + '>.';
+			notify(cfg.slack.channel, msg);
+		}
 	}
 };
 
@@ -160,26 +123,11 @@ function notify(room, msg, sender){
 		text: msg
 		,channel: room
 		,username: sender
-		,icon_url: 'https://slack.global.ssl.fastly.net/10562/img/services/trello_48.png'
 	}, function(err, resp){
 		if (err){
+			throw err;
 			console.error('ERROR:\n', err);
 		}
-	});
-}
-function card_in_lists(card_id, lists, callback){
-	//use cache if it exists
-	if (cachedCardLists.hasOwnProperty(card_id)){
-		return callback(cachedCardLists[card_id]);
-	}
-	//otherwise get the list name
-	trello.get('/1/cards/' + card_id + '/list', function(err, resp){
-		if (err) throw err;
-		var list_name = resp.name;
-
-		var found = (lists.indexOf('*') > -1 || lists.indexOf(list_name) > -1);
-		cachedCardLists[card_id] = found;
-		callback(found);
 	});
 }
 function sanitize(msg){
